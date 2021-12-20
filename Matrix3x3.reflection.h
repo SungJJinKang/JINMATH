ea3dce45dfb144f67ffac8392bb1a827141cb70a
sungJinKangdev <hour30000@gmail.com>
2021-12-21 04:44:27 +0900

    update reflection.h file

--- Matrix3x3.reflection.h
+++ Matrix3x3.reflection.h
@@ -4,13 +4,13 @@
 // SourceFilePath : C:\Doom3FromScratch\Doom3\Source\Core\Math\LightMath_Cpp\Matrix3x3.cpp
 
 
-#ifdef C__Doom3FromScratch_Doom3_Source_Core_Math_LightMath_Cpp_Matrix3x3_reflection_h
+#ifdef _Source_Core_Math_LightMath_Cpp_Matrix3x3_reflection_h
 
-#error "C:\Doom3FromScratch\Doom3\Source\Core\Math\LightMath_Cpp\Matrix3x3.reflection.h already included, missing '#pragma once' in C:\Doom3FromScratch\Doom3\Source\Core\Math\LightMath_Cpp\Matrix3x3.reflection.h"
+#error "_Source_Core_Math_LightMath_Cpp_Matrix3x3_reflection_h already included, missing '#pragma once' in _Source_Core_Math_LightMath_Cpp_Matrix3x3_reflection_h"
 
 #endif
 
-#define C__Doom3FromScratch_Doom3_Source_Core_Math_LightMath_Cpp_Matrix3x3_reflection_h
+#define _Source_Core_Math_LightMath_Cpp_Matrix3x3_reflection_h
 
 
 #include <type_traits>
